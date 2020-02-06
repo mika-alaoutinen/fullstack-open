9484370a50daae3961891aee7a76f0a9a9ea0777
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-06 17:11:46 +0200

    Try stuff in Hiekkalaatikko.

--- hiekkalaatikko/part1/src/index.js
+++ hiekkalaatikko/part1/src/index.js
@@ -1,25 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+// Komponentit:
+const History = ({allClicks}) => {
+  return allClicks.length === 0
+    ? <div>the app is used by pressing the buttons</div>
+    : <div>button press history: {allClicks.join(' ')}</div>
+}
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = (props) => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"))
+    setLeft(left + 1)
+  }
+  
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"))
+    setRight(right + 1)
+  }
+
   return (
-    <>
-      <p>Hei { props.name }, olet { props.age } vuotta vanha. </p>
-    </>
+    <div>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text="left" />
+        <Button onClick={handleRightClick} text="right" />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
+    </div>
   )
 }
 
-const App = () => {
-  const nimi = "Pekka"
-  const ika = 10;
 
-  return (
-    <>
-      <h1>Season's greetings</h1>
-      <Hello name="Mika" age={20+10}/>
-      <Hello name={nimi} age={ika} />
-    </>
-  )
-}
+// const App = (props) => {
+//   const [ counter, setCounter ] = useState(0)
+//   const setToValue = (value) => () => setCounter(value)
+  
+//   return (
+//     <div>
+//       <Display counter={counter} />
+//       <Button onClick={setToValue(counter + 1)} text="plus" />
+//       <Button onClick={setToValue(counter - 1)} text="minus" />
+//       <Button onClick={setToValue(0)} text="zero" />
+//     </div>
+//   )
+// }
+
+// // Komponentit:
+// const Display = ({counter}) => <div>{counter}</div>
+// const Button = ({onClick, text}) => (
+//     <button onClick={onClick}>
+//       {text}
+//     </button>
+//   )
 
 ReactDOM.render(<App />, document.getElementById('root'))
