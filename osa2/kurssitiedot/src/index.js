e23fd9e81d15078063b3a14d75abcd6135e1f3de
Mika Alaoutinen <42463672+mika-alaoutinen@users.noreply.github.com>
2020-02-07 21:04:19 +0200

    Osa2 kurssitiedot (#5)
    * Aloitetaan step 9
    
    * Kurssitiedot valmis.

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,67 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Course from "./components/Course";
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        },
+        {
+          name: 'Redux',
+          exercises: 11
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  const courseRows = courses.map(course =>
+    <Course key={course.name} course={course} />
+  )
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Web development curriculum</h1>
+      {courseRows}
     </div>
-  )
-}
-
-const Header = (props) => {
-  return (
-    <h1>{ props.course }</h1>
-  )
-}
-
-const Content = (props) => {
-  const p1 = props.parts[0];
-  const p2 = props.parts[1];
-  const p3 = props.parts[2];
-
-  return (
-    <div>
-      <Part name={p1.name} exercises={p1.exercises} />
-      <Part name={p2.name} exercises={p2.exercises} />
-      <Part name={p3.name} exercises={p3.exercises} />
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>{ props.name } { props.exercises }</p>
-  )
-}
-
-const Total = (props) => {
-  const sum = props.parts
-      .map(part => part.exercises)
-      .reduce((current, previous) => current + previous, 0)
-
-  return (
-    <p>Number of exercises { sum }</p>
   )
 }
 
