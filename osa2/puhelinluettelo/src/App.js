b5e2740112d49b8f417a70b18a606af7b2b8035d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-08 15:17:12 +0200

    Puhelinluettelo done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import Persons from "./components/Persons";
+import PersonFilter from "./components/PersonFilter";
+import PersonForm from "./components/PersonForm";
 
 const App = () => {
   // State management:
@@ -11,70 +13,28 @@ const App = () => {
   const [ newName, setNewName ] = useState("")
   const [ newPhonenumber, setNewPhonenumber ] = useState("")
   const [ filter, setFilter ] = useState("")
-  const [ filteredPersons, setFilteredPersons ] = useState(persons)
 
-  const addPerson = (event) => {
-    event.preventDefault()
-
-    // Validate new person before adding:
-    if (persons.some(person => person.name === newName)) {
-      window.alert(`${newName} is already added to phonebook`)
-      return
-    }
-    
-    const newPerson = {
-      name: newName,
-      phonenumber: newPhonenumber,
-    }
-
-    setPersons(persons.concat(newPerson))
-    setNewName("")
-    setNewPhonenumber("")
-  }
-
-  const applyFilter = (text) => {
-    setFilter(text.toLowerCase())
-
-    const filtered = persons.filter(person =>
-      person.name.toLowerCase().includes(filter) || person.phonenumber.includes(filter))
-
-    setFilteredPersons(filtered)
-  }
+  const filteredPersons = () => persons.filter(person =>
+      person.name.toLowerCase().includes(filter) ||
+      person.phonenumber.includes(filter))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <PersonFilter filter={filter} setFilter={setFilter} />
 
-      <div>
-        filter shown with
-        <input
-          value={filter}
-          onChange={(event) => applyFilter(event.target.value)}
-        />
-      </div>
-
-      <h2>Add new</h2>
-      <form onSubmit={addPerson}>
-        <div>name:
-          <input
-            value={newName}
-            onChange={(event) => setNewName(event.target.value)}
-          />
-        </div>
-
-        <div>number: 
-          <input
-            value={newPhonenumber}
-            onChange={(event) => setNewPhonenumber(event.target.value)}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-
-      <h2>Numbers</h2>
-      <Persons persons={filteredPersons} />
+      <h3>Add new</h3>
+      <PersonForm
+        persons={persons}
+        setPersons={setPersons}
+        newName={newName}
+        setNewName={setNewName}
+        newPhonenumber={newPhonenumber}
+        setNewPhonenumber={setNewPhonenumber}
+      />
+      
+      <h3>Numbers</h3>
+      <Persons persons={filteredPersons()} />
     </div>
   )
 }
