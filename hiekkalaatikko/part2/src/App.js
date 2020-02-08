b161e6c7c23db05d674592073aa2250a352d438d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-08 11:52:17 +0200

    Osa2 lomakkeet done

--- hiekkalaatikko/part2/src/App.js
+++ hiekkalaatikko/part2/src/App.js
@@ -1,20 +1,54 @@
 import React, { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
   // State management:
   const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState("")
+  const [showAll, setShowAll] = useState(true)
   
-  const rows = () => notes.map(note =>
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  const rows = () => notesToShow.map(note =>
     <Note key={note.id} note={note} />
   )
+
+  const addNote = (event) => {
+    event.preventDefault()
+
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote("")
+  }
 
   return (
     <div>
       <h1>Notes</h1>
+
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all" }
+        </button>
+      </div>
+
       <ul>
         {rows()}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type="submit">save</button>
+      </form> 
     </div>
   )
 }
