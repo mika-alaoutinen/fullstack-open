4cb4d3061deb64cbb98ad02a8ba00cc3580cafb6
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-09 19:14:19 +0200

    Step 9 done

--- osa2/puhelinluettelo/src/components/Persons.js
+++ osa2/puhelinluettelo/src/components/Persons.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import Person from "./Person";
-import service from "../services/personService";
 
 const Persons = ({ persons }) => {
   const personList = persons.map(person =>
-    <div key={person.id}>
-      <Person
-        key={person.name}
-        id={person.id}
-        name={person.name}
-        number={person.number}
-      />
-      <button onClick={() => service.deletePerson(person.id)}>delete</button>
-    </div>
+    <Person
+      key={person.name}
+      id={person.id}
+      name={person.name}
+      number={person.number}
+    />
   )
 
   return <div>{personList}</div>
