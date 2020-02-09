d3e868f8043c787c7f9f713e659c3314fa88da2d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-09 18:18:28 +0200

    Osa 2d demo valmis

--- hiekkalaatikko/part2/src/App.js
+++ hiekkalaatikko/part2/src/App.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from 'react'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   // State management:
@@ -9,17 +10,33 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
   
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => setNotes(response.data))
-  }, [])
+    noteService.getAll()
+      .then(initialNotes => setNotes(initialNotes))
+  })
   
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
 
+  const toggleImportanceOf = id => {
+    const note = notes.find(note => note.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService.update(id, changedNote)
+      .then(returnedNote => setNotes(
+        notes.map(note => note.id !== id ? note : returnedNote)))
+      .catch(error => {
+        alert(`the note '${note.content}' was already deleted from server`)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+    
   const rows = () => notesToShow.map(note =>
-    <Note key={note.id} note={note} />
+    <Note
+      key={note.id}
+      note={note}
+      toggleImportance={() => toggleImportanceOf(note.id)}
+    />
   )
 
   const addNote = (event) => {
@@ -29,11 +46,13 @@ const App = () => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() > 0.5,
-      id: notes.length + 1,
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote("")
+    noteService.create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   }
 
   return (
