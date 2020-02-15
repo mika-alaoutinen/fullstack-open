515b8a12be318bcd82181559f6cd46c4f87678b4
Mika Alaoutinen <42463672+mika-alaoutinen@users.noreply.github.com>
2020-02-15 16:40:37 +0200

    Osa3 tehtävät (#14)
    * T3.12 done
    
    * Osa 3 T 3.13 ja 3.14 done
    
    * Tehtävät 3.15 ja 3.16
    
    * Osa 3 c valmis

--- osa3/index.js
+++ osa3/index.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const cors = require('cors')
+require('dotenv').config()
 const bodyParser = require('body-parser')
-const app = express()
+const cors = require('cors')
+const express = require('express')
 // const morgan = require('morgan')
+const Person = require('./models/person')
+
+const app = express()
 
 // Middleware:
 app.use(express.static('build'))
@@ -11,49 +14,23 @@ app.use(bodyParser.json())
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postdata'))
 // morgan.token('postdata', (req, res) => JSON.stringify(req.body))
 
-// "Database":
-let persons = [
-    {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
-    },
-    {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 2
-    },
-    {
-        name: "Dan Abramov",
-        number: "12-43-234345",
-        id: 3
-    },
-    {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4
-    },
-]
-
 // REST endpoints:
 app.get('/info', (request, response) => {
-    const numPeople = persons.length;
-    const today = new Date();
-
-    response.send(
-        `<p>Phonebook has info for ${numPeople} people</p>
-        ${today}`
-    )
+    Person.find({})
+        .then(people => postInfo(people, response))
 })
 
-app.get('/api/persons', (request, response) =>
-    response.json(persons))
+app.get('/api/persons', (request, response) => {
+    Person.find({})
+        .then(people => response.json(people.map(person => person.toJSON())))
+})
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    person ? response.json(person) : response.status(404).end()
+    Person.findById(request.params.id)
+        .then(person => person
+            ? response.json(person.toJSON())
+            : response.status(404).end())
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -66,26 +43,45 @@ app.post('/api/persons', (request, response) => {
     if (!body.number) {
         return throwError(response, 'number is missing')
     }
-    if (persons.some(person => person.name === body.name)) {
-        return throwError(response, 'name must be unique')
-    }
 
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: Math.floor(Math.random() * Math.floor(100)),
+    })
+
+    person.save()
+        .then(savedPerson => response.json(savedPerson.toJSON()))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const person = {
+        name: request.body.name,
+        number: request.body.number,
     }
 
-    persons = persons.concat(person)
-    response.json(person)
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => response.json(updatedPerson.toJSON()))
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => response.status(204).end())
+        .catch(error => next(error))
 })
+
+// Error handling:
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+        return throwError(response, 'malformatted id')
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 // Utility functions:
 const PORT = process.env.PORT || 3001
@@ -95,3 +91,13 @@ app.listen(PORT, () => {
 
 const throwError = (response, text) =>
     response.status(400).json({error: text})
+
+const postInfo = (people, response) => {
+    const numPeople = people.length;
+    const today = new Date();
+
+    response.send(
+        `<p>Phonebook has info for ${numPeople} people</p>
+        ${today}`
+    )
+}
