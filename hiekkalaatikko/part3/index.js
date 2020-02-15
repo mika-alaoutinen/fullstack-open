8f53d4d09a6b516754a76ac54b5be2a3809fe149
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-15 14:56:56 +0200

    Osa 3c demo done

--- hiekkalaatikko/part3/index.js
+++ hiekkalaatikko/part3/index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const Note = require('./models/note')
 
+// Middleware:
+const app = express()
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
@@ -15,60 +18,39 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(requestLogger)
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2019-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2019-05-30T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2019-05-30T19:20:14.298Z",
-        important: true
-    }
-]
+// Endpoints:
+app.get('/', (request, response) =>
+    response.send('<h1>Hello World!</h1>'))
 
-app.get('/', (req, res) =>
-    res.send('<h1>Hello World!</h1>'))
-
-app.get('/notes', (req, res) =>
-    res.json(notes))
+app.get('/notes', (request, response) => {
+    Note.find({}).then(notes => {
+        response.json(notes.map(note => note.toJSON()))
+    })
+})
 
 app.get('/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    note ? response.json(note) : response.status(404).end()
+    Note.findById(request.params.id)
+        .then(note => response.json(note.toJSON()))
 })
 
 app.post('/notes', (request, response) => {
     const body = request.body
 
     if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+        return response.status(400).json({error: 'content missing'})
     }
 
-    const note = {
-        id: generateId(),
+    const note = new Note({
         content: body.content,
         date: new Date(),
         important: body.important || false,
-    }
+    })
 
-    notes = notes.concat(note)
-    response.json(note)
+    note.save()
+        .then(savedNote => response.json(savedNote.toJSON()))
 })
 
+// TODO: fix
 app.delete('/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
@@ -82,14 +64,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
-
-// Utility functions:
-const generateId = () => {
-    return notes.length > 0
-        ? Math.max(...notes.map(n => n.id)) + 1
-        : 1
-}
