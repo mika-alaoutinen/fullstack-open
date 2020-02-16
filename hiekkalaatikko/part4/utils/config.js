83e563edc0b2c7b90420cf7df297d687f4baaad7
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-16 18:29:26 +0200

    Osan 4b demon testausasiat

--- hiekkalaatikko/part4/utils/config.js
+++ hiekkalaatikko/part4/utils/config.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URL = process.env.DB_URL
+let MONGODB_URL = process.env.DB_URL
+
+if (process.env.NODE_ENV === 'test') {
+    MONGODB_URL = process.env.TEST_DB_URL
+}
 
 module.exports = { MONGODB_URL, PORT }
