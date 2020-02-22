759c98b3f9514e486079b20746479ced95b543c0
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-22 18:17:16 +0200

    Osa 4 c done

--- hiekkalaatikko/part4/controllers/notes.js
+++ hiekkalaatikko/part4/controllers/notes.js
@@ -1,8 +1,12 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-    const notes = await Note.find({})
+    const notes = await Note
+        .find({})
+        .populate('user', { username: 1, name: 1 })
+        
     response.json(notes.map(note => note.toJSON()))
 })
 
@@ -18,14 +22,21 @@ notesRouter.get('/:id', async (request, response, next) => {
 })
 
 notesRouter.post('/', async (request, response, next) => {
+    const body = request.body
+
+    const user = await User.findById(body.userId)
+
     const note = new Note({
-        content: request.body.content,
-        important: request.body.important || false,
+        content: body.content,
+        important: body.important || false,
         date: new Date(),
+        user: user._id
     })
 
     try {
         const savedNote = await note.save()
+        user.notes = user.notes.concat(savedNote._id)
+        await user.save()
         response.json(savedNote.toJSON())
     } catch (exception) {
         next(exception)
