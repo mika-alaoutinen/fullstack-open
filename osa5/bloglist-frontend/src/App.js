07a19bbf27dc0cb3ee5f41025c0a2dca8335ec01
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-26 16:57:30 +0200

    Osa 5a T 5.2 done

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import BlogList from "./components/BlogList";
 import LoginForm from "./components/LoginForm";
+import blogService from "./services/blogService";
 
 const App = () => {
   // State management:
@@ -9,6 +10,21 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
+  
+  // Check if user credentials are in local storage:
+  useEffect(() => {
+    const loggedUser = window.localStorage.getItem('user')
+    if (loggedUser) {
+      const user = JSON.parse(loggedUser)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+  
+  const logout = () => () => {
+    window.localStorage.clear()
+    window.location.reload()
+  }
   
   return (
     <div>
@@ -19,7 +35,14 @@ const App = () => {
             user={user} setUser={setUser}
             setErrorMessage={setErrorMessage}
           />
-        : <BlogList blogs={blogs} setBlogs={setBlogs} />
+        : <div>
+            <h2>blogs</h2>
+            <p>
+              {user.name} logged in
+              <button onClick={logout()}>logout</button>
+            </p>
+            <BlogList blogs={blogs} setBlogs={setBlogs} />
+          </div>
       }
     </div>
   )
--- osa5/bloglist-frontend/src/components/BlogList.js
+++ osa5/bloglist-frontend/src/components/BlogList.js
@@ -8,14 +8,7 @@ const BlogList = ({ blogs, setBlogs }) => {
     blogService.getAll().then(blogs => setBlogs(blogs))
   })
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+  return blogs.map(blog => <Blog key={blog.id} blog={blog} />)
 }
 
 export default BlogList
