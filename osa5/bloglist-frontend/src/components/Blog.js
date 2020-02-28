15626416085181f1bafb5b9dbbe3ef7249fd9eba
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-28 19:39:48 +0200

    Typerä ratkaisu osa 5b T 5.6

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+
+  // Expand and collapse blog info:
+  const [visible, setVisible] = useState(false)
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+  const toggleVisibility = () => setVisible(!visible)
+
+  const addLike = () => () => {
+    console.log('+1 like');
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={toggleVisibility} style={hideWhenVisible}>
+        {blog.title} {blog.author}
+      </div>
+      
+      <div onClick={toggleVisibility} style={showWhenVisible}>
+        <div>{blog.title} {blog.author}</div>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes} likes
+          <button onClick={addLike()}>like</button>
+        </div>
+        <div>added by {blog.user.name}</div>
+      </div>
+      
+    </div>
+  )
+}
 
 export default Blog
