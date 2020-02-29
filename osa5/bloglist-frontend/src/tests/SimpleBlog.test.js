11bde007e1fe71421ee7f7fa2e1c22d7774b24b0
Mika Alaoutinen <42463672+mika-alaoutinen@users.noreply.github.com>
2020-02-29 16:34:41 +0200

    Osa 5c (#22)
    * Ekat testausdemot tehty
    
    * Siirretty testit tests-kansioon
    
    * Osa 5c T 5.13 tehty
    
    * Osa 5c T5.14 tehty
    
    * Osa 5c T5.15 tehty
    
    * Integraatiotesti
    
    * Osa 5c valmis

---
+++ osa5/bloglist-frontend/src/tests/SimpleBlog.test.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import SimpleBlog from '../components/SimpleBlog'
+
+// Global helper variables:
+let component
+
+const likeHandler = jest.fn()
+
+const blog = {
+  title: 'SimpleBlog test',
+  author: 'Pekka Kana',
+  likes: 3,
+}
+
+beforeEach(() => {
+  component = render(
+    <SimpleBlog blog={blog} onClick={likeHandler} />
+  )
+})
+
+describe('SimpleBlog renders correctly', () => {
+  test('renders blog title', () => {
+    expect(component.container).toHaveTextContent('SimpleBlog test')
+  })
+
+  test('renders blog author', () => {
+    expect(component.container).toHaveTextContent('Pekka Kana')
+  })
+
+  test('blog has 3 likes', () => {
+    expect(component.container).toHaveTextContent('blog has 3 likes')
+  })
+})
+
+describe('Like button works correctly', () => {
+  test('Clicking like button twice calls event handler twice', () => {
+    const { getByText } = component
+    const button = getByText('like')
+
+    fireEvent.click(button)
+    fireEvent.click(button)
+
+    expect(likeHandler.mock.calls.length).toBe(2)
+  })
+})
