27489f5867bc241c2cbdba0600b0f3fda242a2f5
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-29 13:08:26 +0200

    Osa 5c T5.14 tehty

--- osa5/bloglist-frontend/src/tests/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/tests/SimpleBlog.test.js
@@ -1,24 +1,23 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, prettyDOM } from '@testing-library/react'
 import SimpleBlog from '../components/SimpleBlog'
 
+// Global helper variables:
+let component
+const likeHandler = jest.fn()
+const blog = {
+  title: 'SimpleBlog test',
+  author: 'Pekka Kana',
+  likes: 3,
+}
+
+beforeEach(() => {
+  component = render(
+    <SimpleBlog blog={blog} onClick={likeHandler} />
+  )
+})
+
 describe('SimpleBlog renders correctly', () => {
-  let component
-
-  beforeEach(() => {
-    const blog = {
-      title: 'SimpleBlog test',
-      author: 'Pekka Kana',
-      likes: 3,
-    }
-
-    const likeHandler = () => console.log('+1 like')
-
-    component = render(
-      <SimpleBlog blog={blog} onClick={likeHandler} />
-    )
-  })
-
   test('renders blog title', () => {
     expect(component.container).toHaveTextContent('SimpleBlog test')
   })
@@ -29,5 +28,17 @@ describe('SimpleBlog renders correctly', () => {
 
   test('blog has 3 likes', () => {
     expect(component.container).toHaveTextContent('blog has 3 likes')
+  })
+})
+
+describe('Like button works correctly', () => {
+  test('Clicking like button twice calls event handler twice', () => {
+    const { getByText } = component
+    const button = getByText('like')
+
+    fireEvent.click(button)
+    fireEvent.click(button)
+
+    expect(likeHandler.mock.calls.length).toBe(2)
   })
 })
