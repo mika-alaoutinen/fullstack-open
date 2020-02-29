00194d8c4cc10d7e34e5619ac6aae30a23b186b7
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-29 20:10:47 +0200

    Osa 5d T 5.19 tehty

--- osa5/bloglist-frontend/src/hooks/index.js
+++ osa5/bloglist-frontend/src/hooks/index.js
@@ -3,9 +3,13 @@ import { useState } from 'react'
 export const useField = type => {
   const [value, setValue] = useState('')
 
-  const onChange = (event) => {
+  const onChange = event => {
     setValue(event.target.value)
   }
 
-  return { type, value, onChange }
+  const reset = () => {
+    setValue('')
+  }
+
+  return { type, value, onChange, reset }
 }
--- osa5/bloglist-frontend/src/services/loginService.js
+++ osa5/bloglist-frontend/src/services/loginService.js
@@ -1,11 +1,8 @@
 import axios from 'axios'
 const baseUrl = '/api/login'
 
-const login = credentials => {
-  console.log('creds', credentials);
-
-  return axios.post(baseUrl, credentials)
+const login = credentials =>
+  axios.post(baseUrl, credentials)
     .then(response => response.data)
-}
 
 export default { login }
