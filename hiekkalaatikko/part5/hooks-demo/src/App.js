5a8adda3e187d61ee20286f979945b87e80b8d34
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-29 17:44:19 +0200

    hooks-demo tehty

--- hiekkalaatikko/part5/hooks-demo/src/App.js
+++ hiekkalaatikko/part5/hooks-demo/src/App.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './components/Button'
 import Display from './components/Display'
 import useCounter from "./hooks/useCounter";
+import useField from "./hooks/useField";
+
+// const App = () => {
+//   const counter = useCounter()
+
+//   return (
+//     <div>
+//       <Display counter={counter.value} />
+//       <Button onClick={counter.increase} text='plus' />
+//       <Button onClick={counter.decrease} text='minus' />
+//       <Button onClick={counter.zero} text='zero' />
+//     </div>
+//   )
+// }
 
 const App = () => {
-  const counter = useCounter()
+  const name = useField('text')
+  const birthdate = useField('date')
+  const height = useField('number')
 
   return (
     <div>
-      <Display counter={counter.value} />
-      <Button onClick={counter.increase} text='plus' />
-      <Button onClick={counter.decrease} text='minus' />
-      <Button onClick={counter.zero} text='zero' />
+      <form>
+        name: <input { ...name } />
+        <br/> 
+
+        birthdate: <input { ...birthdate } />
+        <br /> 
+
+        height: <input { ...height } />
+      </form>
+      
+      <div>
+        {name.value} {birthdate.value} {height.value} 
+      </div>
     </div>
   )
 }
