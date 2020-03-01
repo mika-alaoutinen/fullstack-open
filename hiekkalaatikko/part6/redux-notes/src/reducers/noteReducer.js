c3ec8c41f18c16605d645d47198ea877106e3494
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-03-01 12:39:54 +0200

    Minor refactor

--- hiekkalaatikko/part6/redux-notes/src/reducers/noteReducer.js
+++ hiekkalaatikko/part6/redux-notes/src/reducers/noteReducer.js
@@ -1,18 +1,25 @@
 const noteReducer = (state = [], action) => {
   switch (action.type) {
     case 'NEW_NOTE':
-      return [ ...state, action.data ]
+      return addNote(state, action)
     case 'TOGGLE_IMPORTANCE':
-      const id = action.data.id
-      const noteToChange = state.find(note => note.id === id)
-      const changedNote = {
-        ...noteToChange,
-        important: !noteToChange.important
-      }
-      return state.map(note => note.id === id ? changedNote : note)
+      return toggleImportance(state, action)
     default:
       return state
   }
+}
+
+const addNote = (state, action) => [ ...state, action.data ]
+
+const toggleImportance = (state, action) => {
+  const id = action.data.id
+  const noteToChange = state.find(note => note.id === id)
+  const changedNote = {
+    ...noteToChange,
+    important: !noteToChange.important
+  }
+
+  return state.map(note => note.id === id ? changedNote : note)
 }
 
 export default noteReducer
