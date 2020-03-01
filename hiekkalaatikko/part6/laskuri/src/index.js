03758ccdb37edd05e521d710d819be7b829c72c3
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-03-01 11:48:04 +0200

    Redux-laskuri

--- hiekkalaatikko/part6/laskuri/src/index.js
+++ hiekkalaatikko/part6/laskuri/src/index.js
@@ -1,5 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      return state
+  }
+}
+
+// Redux-store:
+const store = createStore(counterReducer)
+
+const renderApp = () => {
+  ReactDOM.render(<App store={store} />, document.getElementById('root'));
+}
+
+renderApp()
+store.subscribe(renderApp)
