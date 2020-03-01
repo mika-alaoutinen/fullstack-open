58c7702b42d6fc99037e2a94106bc41f6765d79f
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-03-01 13:41:10 +0200

    Refactor tests

--- osa6/unicafe-redux/src/reducer.test.js
+++ osa6/unicafe-redux/src/reducer.test.js
@@ -2,67 +2,40 @@ import deepFreeze from 'deep-freeze'
 import counterReducer from './reducer'
 
 describe('unicafe reducer', () => {
-  const initialState = {
-    good: 0,
-    ok: 0,
-    bad: 0
-  }
+  const initialState = { good: 0, ok: 0, bad: 0 }
 
   test('should return a proper initial state when called with undefined state', () => {
-    const state = {}
-    const action = {
-      type: 'DO_NOTHING'
-    }
+    const action = { type: 'DO_NOTHING' }
 
     const newState = counterReducer(undefined, action)
     expect(newState).toEqual(initialState)
   })
 
   test('good is incremented', () => {
-    const action = { type: 'GOOD' }
-    const state = initialState
-
-    deepFreeze(state)
-    const newState = counterReducer(state, action)
-    expect(newState).toEqual({
-      good: 1,
-      ok: 0,
-      bad: 0
-    })
+    const newState = stateTestHelper('GOOD')
+    expect(newState).toEqual({ good: 1, ok: 0, bad: 0 })
   })
 
   test('ok is incremented', () => {
-    const action = { type: 'OK' }
-    const state = initialState
-
-    deepFreeze(state)
-    const newState = counterReducer(state, action)
-    expect(newState).toEqual({
-      good: 0,
-      ok: 1,
-      bad: 0
-    })
+    const newState = stateTestHelper('OK')
+    expect(newState).toEqual({ good: 0, ok: 1, bad: 0 })
   })
 
   test('bad is incremented', () => {
-    const action = { type: 'BAD' }
-    const state = initialState
-
-    deepFreeze(state)
-    const newState = counterReducer(state, action)
-    expect(newState).toEqual({
-      good: 0,
-      ok: 0,
-      bad: 1
-    })
+    const newState = stateTestHelper('BAD')
+    expect(newState).toEqual({ good: 0, ok: 0, bad: 1 })
   })
 
   test('state is reset', () => {
-    const action = { type: 'ZERO' }
+    const newState = stateTestHelper('ZERO')
+    expect(newState).toEqual(initialState)
+  })
+
+  const stateTestHelper = (type) => {
+    const action = { type: type }
     const state = initialState
 
     deepFreeze(state)
-    const newState = counterReducer(state, action)
-    expect(newState).toEqual(initialState)
-  })
+    return counterReducer(state, action)
+  }
 })
