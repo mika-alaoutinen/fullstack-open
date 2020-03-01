81b39cd5a8640e346a77a844f3b9f63f96c5846e
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-03-01 11:12:26 +0200

    Alustettu osan 6 projektit

--- osa5/src/components/BlogList.js
+++ osa5/src/components/BlogList.js
@@ -9,9 +9,11 @@ const BlogList = ({ blogs, setBlogs }) => {
     blogService.getAll().then(blogs => setBlogs(blogs))
   })
 
-  return blogs
-    .sort((blog1, blog2) => blog2.likes - blog1.likes)
-    .map(blog => <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} />)
+  return blogs === undefined
+    ? []
+    : blogs
+        .sort((blog1, blog2) => blog2.likes - blog1.likes)
+        .map(blog => <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} />)
 }
 
 BlogList.propTypes = {
