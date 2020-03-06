71f49e33e5af5eeea69cb6f4494970922fa206ee
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-03-06 18:19:49 +0200

    Osa 6c T6.16

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdoteService'
+import { initAnecdotes } from './reducers/anecdoteReducer'
 
-const App = () => (
-  <div>
-    <h2>Anecdotes</h2>
-    <Notification />
-    <Filter />
-    <AnecdoteList />
-    <AnecdoteForm />
-  </div>
-)
+const App = ({ initAnecdotes }) => {
+  useEffect(() => {
+    anecdoteService.getAll()
+      .then(anecdotes => initAnecdotes(anecdotes))
+  })
 
-export default App
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Notification />
+      <Filter />
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default connect(null, { initAnecdotes })(App)
