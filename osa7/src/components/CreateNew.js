a692980776e200057d673cd36f06628a6aeab7fd
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-03-14 19:55:26 +0200

    Lisätty eslint

--- osa7/src/components/CreateNew.js
+++ osa7/src/components/CreateNew.js
@@ -24,15 +24,15 @@ const CreateNew = ({ anecdotes, setAnecdotes, setNotification, history }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
         </div>
         <div>
           author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
         </div>
         <div>
           url for more info
-            <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
         </div>
         <button>create</button>
       </form>
