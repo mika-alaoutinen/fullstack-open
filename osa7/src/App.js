4db4c9439b22d5d83153bc92335181ef24b76ab3
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-03-14 17:07:28 +0200

    Move test data to file

--- osa7/src/App.js
+++ osa7/src/App.js
@@ -1,24 +1,12 @@
 import React, { useState } from 'react'
+import { initialAnecdotes } from './initialAnecdotes'
 import Footer from './components/Footer'
 import Menu from './components/Menu'
 
+console.log(initialAnecdotes);
+
 const App = () => {
-  const [anecdotes, setAnecdotes] = useState([
-    {
-      content: 'If it hurts, do it more often',
-      author: 'Jez Humble',
-      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
-      votes: 0,
-      id: '1'
-    },
-    {
-      content: 'Premature optimization is the root of all evil',
-      author: 'Donald Knuth',
-      info: 'http://wiki.c2.com/?PrematureOptimization',
-      votes: 0,
-      id: '2'
-    }
-  ])
+  const [anecdotes, setAnecdotes] = useState(initialAnecdotes)
 
   const anecdoteById = (id) =>
     anecdotes.find(a => a.id === id)
