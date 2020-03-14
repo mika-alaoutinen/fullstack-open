81b777834c62c15344140cb68f66aef0fcbd6dac
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-03-14 15:37:25 +0200

    Notes refactor

--- hiekkalaatikko/part7/notes-frontend/src/App.js
+++ hiekkalaatikko/part7/notes-frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Link, Redirect, withRouter } from 'reac
 import Footer from "./components/Footer"
 import Home from "./components/Home"
 import LoginForm from "./components/LoginForm"
-import Note from './components/Note'
 import NoteForm from "./components/NoteForm"
 import Notes from "./components/Notes"
 import Notification from "./components/Notification"
@@ -36,37 +35,11 @@ const App = () => {
       noteService.setToken(user.token)
     }
   }, [])
-  
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important)
-
-  const toggleImportanceOf = id => {
-    const note = notes.find(note => note.id === id)
-    const changedNote = { ...note, important: !note.important }
-  
-    noteService.update(id, changedNote)
-      .then(returnedNote => setNotes(notes.map(
-        note => note.id !== id ? note : returnedNote)))
-      .catch(() => {
-        setErrorMessage(`Note '${note.content}' was already removed from server`)
-        setTimeout(() => setErrorMessage(null), 2000);
-        setNotes(notes.filter(note => note.id !== id))
-      })
-  }
 
   const logout = () => {
     window.localStorage.clear()
     window.location.reload()
   }
-
-  const rows = () => notesToShow.map(note =>
-    <Note
-      key={note.id}
-      note={note}
-      toggleImportance={() => toggleImportanceOf(note.id)}
-    />
-  )
 
   const noteFormRef = React.createRef()
 
@@ -83,13 +56,20 @@ const App = () => {
             <Link style={padding} to="/users">users</Link>
           </div>
           <Route exact path="/" render={() => <Home />} />
-          <Route path="/notes" render={() => <Notes />} />
+
+          <Route path="/notes" render={() =>
+            <Notes
+              notes={notes} setNotes={setNotes}
+              showAll={showAll} setShowAll={setShowAll}
+              setErrorMessage={setErrorMessage}
+            />}
+          />
+          
           <Route path="/users" render={() => <Users />} />
         </div>
       </Router>
 
       <div>
-        <h1>Notes</h1>
         <Notification message={errorMessage} />
 
         {user === null
@@ -111,14 +91,6 @@ const App = () => {
               </Toggleable>
             </div>
         }
-
-        <div>
-          <button onClick={() => setShowAll(!showAll)}>
-            show {showAll ? "important" : "all" }
-          </button>
-        </div>
-
-        <ul>{rows()}</ul>
 
         <Footer />
       </div>
--- hiekkalaatikko/part7/notes-frontend/src/components/Notes.js
+++ hiekkalaatikko/part7/notes-frontend/src/components/Notes.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import Note from './Note'
+import noteService from '../services/noteService'
 
-const Notes = () => (
-  <div>
-    <h2>Notes</h2>
-  </div>
-)
+const Notes = ({ notes, setNotes, showAll, setShowAll, setErrorMessage }) => {
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(note => note.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService.update(id, changedNote)
+      .then(returnedNote => setNotes(notes.map(
+        note => note.id !== id ? note : returnedNote)))
+      .catch(() => {
+        setErrorMessage(`Note '${note.content}' was already removed from server`)
+        setTimeout(() => setErrorMessage(null), 2000);
+        setNotes(notes.filter(note => note.id !== id))
+      })
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+  
+  const renderNotes = () =>
+    notesToShow.map(note => 
+      <Note
+        key={note.id}
+        note={note}
+        toggleImportance={() => toggleImportanceOf(note.id)}
+      />
+    )
+  
+  return (
+    <div>
+      <h2>Notes</h2>
+      { renderNotes() }
+      
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? "important" : "all" }
+      </button>
+    </div>
+  )
+}
 
 export default Notes
