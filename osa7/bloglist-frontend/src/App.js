e36f94704d8be054b6e75788ff498cf56b377859
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-03-20 18:50:27 +0200

    App useEffect refaktoroitu

--- osa7/bloglist-frontend/src/App.js
+++ osa7/bloglist-frontend/src/App.js
@@ -1,33 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import BlogPage from './components/BlogPage'
 import LoginForm from './components/LoginForm'
-import blogService from './services/blogService'
 import { initBlogs } from './reducers/blogReducer'
-import { userLogin, setUser } from './reducers/userReducer'
+import { setUser } from './reducers/userReducer'
 import { useField } from './hooks/index'
 
 const App = () => {
   const dispatch = useDispatch()
-
-  // Check if user credentials are in local storage:
+  
   useEffect(() => {
+    checkLoggedUser()       // Check if user credentials are in local storage:
+    dispatch(initBlogs())   // Retrieve all blogs from the server
+  }, [dispatch])
+
+  const checkLoggedUser = () => {
     const loggedUser = window.localStorage.getItem('user')
     if (loggedUser) {
       const user = JSON.parse(loggedUser)
       dispatch(setUser(user))
-    }
-  }, [dispatch])
-
-  // Retrieve all blogs from the server:
-  useEffect(() => {
-    dispatch(initBlogs())
-  }, [dispatch])
+    }    
+  }
 
   // Custom state management hooks:
   const { reset: resetUsername, ...username } = useField('text')
   const { reset: resetPassword, ...password } = useField('password')
-  
+
   const user = useSelector(state => state.user)
 
   return (
