0b53e1fe1873ec377662619a88f596a8a2ecb8a4
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-03-22 18:21:19 +0200

    Demo part 8-2

--- hiekkalaatikko/part8/index.js
+++ hiekkalaatikko/part8/index.js
@@ -1,35 +1,22 @@
-const { ApolloServer, gql } = require('apollo-server')
-
-let persons = [
-  {
-    name: "Arto Hellas",
-    phone: "040-123543",
-    street: "Tapiolankatu 5 A",
-    city: "Espoo",
-    id: "3d594650-3436-11e9-bc57-8b80ba54c431"
-  },
-  {
-    name: "Matti Luukkainen",
-    phone: "040-432342",
-    street: "Malminkaari 10 A",
-    city: "Helsinki",
-    id: '3d599470-3436-11e9-bc57-8b80ba54c431'
-  },
-  {
-    name: "Venla Ruuska",
-    street: "Nallemäentie 22 C",
-    city: "Helsinki",
-    id: '3d599471-3436-11e9-bc57-8b80ba54c431'
-  },
-]
+const { ApolloServer, UserInputError, gql } = require('apollo-server')
+const uuid = require('uuid/v1')
+let persons = require('./persons')
 
 const typeDefs = gql`
+  type Address {
+    street: String!
+    city: String! 
+  }
+
   type Person {
     name: String!
     phone: String
-    street: String!
-    city: String! 
+    address: Address!
     id: ID!
+  }
+
+  type Mutation {
+    addPerson(name: String! phone: String street: String! city: String!): Person
   }
 
   type Query {
@@ -40,12 +27,25 @@ const typeDefs = gql`
 `
 
 const resolvers = {
+  Mutation: {
+    addPerson: (root, args) => {
+      validateNewPerson(args)
+      return addNewPerson(args)
+    }
+  },
+  
   Query: {
     personCount: () => persons.length,
     allPersons: () => persons,
-    findPerson: (root, args) =>
-      persons.find(p => p.name === args.name)
-  }
+    findPerson: (root, args) => persons.find(p => p.name === args.name),
+  },
+
+  Person: {
+    address: root => ({
+      street: root.street,
+      city: root.city,
+    })
+  },
 }
 
 const server = new ApolloServer({
@@ -56,3 +56,18 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
+
+// Utility functions:
+const addNewPerson = person => {
+  const newPerson = { ...person, id: uuid() }
+  persons = persons.concat(newPerson)
+  return newPerson
+}
+
+const validateNewPerson = (person) => {
+  if (persons.find(p => p.name === person.name)) {
+    throw new UserInputError('Name must be unique', {
+      invalidArgs: person.name,
+    })
+  }
+}
