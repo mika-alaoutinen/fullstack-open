7742f33ff9732f5afc858dca65575273e04924f1
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-03-23 17:46:47 +0200

    Osa 8-a T1-3

--- osa8/index.js
+++ osa8/index.js
@@ -3,12 +3,38 @@ const authors = require('./data/authors')
 const books = require('./data/books')
 
 const typeDefs = gql`
+  type Author {
+    name: String!
+    born: Int!
+    bookCount: Int!
+    id: ID!
+  }
+
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+  }
+
   type Query {
+    authorCount: Int!
+    bookCount: Int!
+    allAuthors: [Author!]!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
+    authorCount: () => authors.length,
+    bookCount: () => books.length,
+    allAuthors: () => authors,
+    allBooks: () => books,
+  },
+
+  Author: {
+    bookCount: root => books.filter(b => b.author === root.name).length,
   }
 }
 
@@ -20,3 +46,5 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
+
+// Utility functions
