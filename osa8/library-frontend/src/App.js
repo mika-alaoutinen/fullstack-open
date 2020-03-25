37a9d3cedf57b7961b672f9fef85f829d97822eb
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-03-25 17:38:33 +0200

    Oikea repo kloonattu

--- osa8/library-frontend/src/App.js
+++ osa8/library-frontend/src/App.js
@@ -1,20 +1,34 @@
-import React from 'react'
-import { gql, useQuery } from '@apollo/client'
 
-const ALL_PERSONS = gql`
-  query {
-    allPersons  {
-      name
-      phone
-      id
-    }
-  }
-`
+import React, { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
 
 const App = () => {
-  const result = useQuery(ALL_PERSONS)
+  const [page, setPage] = useState('authors')
 
-  return <div>loading...</div>
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        show={page === 'authors'}
+      />
+
+      <Books
+        show={page === 'books'}
+      />
+
+      <NewBook
+        show={page === 'add'}
+      />
+
+    </div>
+  )
 }
 
 export default App
