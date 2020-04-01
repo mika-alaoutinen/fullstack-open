07a799ebfbdb2a5c8235ebe8c4967338cdf3d64b
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-04-01 20:19:03 +0300

    Demo lisätty

---
+++ hiekkalaatikko/part9/calculator.ts
@@ -0,0 +1,23 @@
+type Operation = 'multiply' | 'add' | 'divide';
+type Result = number | string
+
+const calculator = (a: number, b: number, op : Operation): Result => {
+  switch (op) {
+    case 'multiply':
+      return a * b;
+    case 'add':
+      return a + b;
+    case 'divide':
+      if (b === 0) {
+        throw new Error('Can\'t divide by 0!');
+      }
+      return a / b;
+    default:
+      throw new Error('Operation is not multiply, add or divide!');
+  }
+}
+
+const a: number = Number(process.argv[2])
+const b: number = Number(process.argv[3])
+
+calculator(a, b, 'divide');
