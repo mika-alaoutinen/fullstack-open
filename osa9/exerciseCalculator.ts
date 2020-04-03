4c24ba42cd4bb5759c9408697aa2de5077f3f23b
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-04-03 19:29:52 +0300

    Osa 9b T 2 ja 3

---
+++ osa9/exerciseCalculator.ts
@@ -0,0 +1,50 @@
+interface Result {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number,
+}
+
+const calculateExercises = (targetHours: number, excerciseHours: Array<number>): Result => {
+  const calculateRating = (target: number, result: number): number => {
+    const performance = result - target;
+    if (performance > 0) return 3;
+    else if (performance > -0.5) return 2;
+    else return 1;
+  }
+  
+  const average = excerciseHours.reduce((total, accumulator) => total + accumulator, 0) / excerciseHours.length;
+  const rating = calculateRating(targetHours, average);
+  const descriptions = [ 'step up senpai', 'not too bad but could be better', 'very nice!' ];
+
+  return {
+    periodLength: excerciseHours.length,
+    trainingDays: excerciseHours.filter(number => number !== 0).length,
+    success: average < targetHours,
+    rating: rating,
+    ratingDescription: descriptions[ rating - 1 ],
+    target: targetHours,
+    average: average,
+  }
+}
+
+const parseArguments = (args: Array<string>) => {
+  const [ targetStr, ...excerciseHoursStr ] = args;
+  const target = parseInt(targetStr);
+  const excerciseHours = excerciseHoursStr.map(number => parseInt(number))
+
+  if (isNaN(target) || excerciseHours.some(number => isNaN(number))) {
+    throw new Error('Not a number');
+  }
+
+  return { target, excerciseHours }
+}
+
+// Driver code:
+const args = process.argv.slice(2);
+const { target, excerciseHours } = parseArguments(args);
+const result = calculateExercises(target, excerciseHours);
+console.log(result);
