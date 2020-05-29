c048e3e982ff755bdf41ef4c4a055757bbed5872
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-29 18:57:16 +0300

    Demo käyty läpi ex 9.10 asti

---
+++ hiekkalaatikko/part9c/src/services/diaryService.ts
@@ -0,0 +1,18 @@
+import diaries from '../../data';
+import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+
+const getEntries = (): Array<DiaryEntry> => {
+  return diaries;
+};
+
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => ({
+    id, date, weather, visibility, })
+  );
+}
+
+const addEntry = () => {
+  return null;
+};
+
+export default { getEntries, getNonSensitiveEntries, addEntry };
