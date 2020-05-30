8ee50afcc6afc7f82482b5b0c909669ec8ddaac7
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-30 10:33:47 +0300

    Osan 9c demot tehty

--- hiekkalaatikko/part9/flight_diaries/src/services/diaryService.ts
+++ hiekkalaatikko/part9/flight_diaries/src/services/diaryService.ts
@@ -1,18 +1,24 @@
-import diaries from '../../data';
-import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+import diaries from '../../data/data';
+import { DiaryEntry, NewDiaryEntry, NonSensitiveDiaryEntry } from '../types';
 
-const getEntries = (): Array<DiaryEntry> => {
-  return diaries;
+const getEntries = (): Array<DiaryEntry> => diaries;
+
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] =>
+  diaries;
+
+const findById = (id: number): DiaryEntry | undefined =>
+  diaries.find(d => d.id === id);
+
+const addEntry = (entry: NewDiaryEntry): DiaryEntry => {
+  const diary = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry
+  }
+  
+  diaries.push(diary);
+  return diary;
 };
 
-const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
-  return diaries.map(({ id, date, weather, visibility }) => ({
-    id, date, weather, visibility, })
-  );
-}
-
-const addEntry = () => {
-  return null;
+export default {
+  getEntries, getNonSensitiveEntries, findById, addEntry
 };
-
-export default { getEntries, getNonSensitiveEntries, addEntry };
